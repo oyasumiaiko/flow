bd4a3adb9e3990833abf915c774850dc71be8f12
見 <117180266+oyasumiaiko@users.noreply.github.com>
2026-08-24 14:06:17 +0800

    fix(reader): stabilize continuous mobile reading

--- packages/epubjs/src/index.js
+++ packages/epubjs/src/index.js
@@ -4,6 +4,7 @@ import ePub from './epub'
 import EpubCFI from './epubcfi'
 import Layout from './layout'
 import Rendition from './rendition'
+import StableViewManager from './managers/stable'
 
 export default ePub
-export { Book, EpubCFI, Rendition, Contents, Layout }
+export { Book, EpubCFI, Rendition, Contents, Layout, StableViewManager }
